253d8ee0ade6e61209b07d7dbb6619d8d4def65b
sundirrala <nisara.drahman@gmail.com>
2020-12-13 17:15:57 -0500

    slime moving with force and button for player attack

---
+++ Assets/Scripts/AttackButtonBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttackButtonBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    float time = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.GetComponent<PlayerBehaviour>().isAttacc)
+        {
+            time += Time.deltaTime;
+        }
+
+        if (time >= 1.583f && player.GetComponent<PlayerBehaviour>().isAttacc)
+        {
+            time = 0.0f;
+            player.GetComponent<PlayerBehaviour>().isAttacc = false;
+        }
+
+
+
+        Debug.Log(player.GetComponent<PlayerBehaviour>().isAttacc.ToString());
+    }
+
+    public void onAttackButtPressed()
+    {
+        Debug.Log("FIrE");
+        player.GetComponent<PlayerBehaviour>().isAttacc = true;
+        player.GetComponent<Animator>().SetInteger("state", 3);
+    }
+}
---
+++ Assets/Scripts/SlimeBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeBehaviour : MonoBehaviour
+{
+    public float force;
+    public Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        rigidbody2D.AddForce(Vector2.left * force * Time.deltaTime);
+    }
+}
