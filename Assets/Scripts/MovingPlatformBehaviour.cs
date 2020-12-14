af6d261e86c4a4d5e91c315179f05e39022194ed
sundirrala <nisara.drahman@gmail.com>
2020-12-13 22:19:24 -0500

    pingpong moving platform

---
+++ Assets/Scripts/MovingPlatformBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformBehaviour : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+
+    private Vector3 distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = end.position - start.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        transform.position = new Vector3(start.position.x + Mathf.PingPong(Time.time, distance.x), start.position.y, 0.0f);
+    }
+}
